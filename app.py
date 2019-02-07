be3126098a4bade720840cba7ca533d0a71466f0
Agar3s <ekeisco@gmail.com>
2019-02-07 11:15:43 -0500

    boot scene wip

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ def run_game(width, height, fps, starting_scene):
 
 if __name__ == '__main__':
     comm.init_connections('COM5', 'COM6')
-    run_game(1280, 720, 60, TutorialScene)
-    #run_game(1280, 720, 60, BootScene)
+    #run_game(1280, 720, 60, TutorialScene)
+    run_game(1280, 720, 60, BootScene)
--- scenes/TutorialScene.py
+++ scenes/TutorialScene.py
@@ -23,8 +23,30 @@ class TutorialScene(SceneBase):
         self.printer02.setAnchor(0.5, 1)
         self.printer03.setAnchor(0.5, 1)
 
+        self.materialL1 = utils.Sprite('assets/sprites/mtlL1.png', 301, 292)
+        self.materialL2 = utils.Sprite('assets/sprites/mtlL2.png', 301, 365)
+        self.materialL3 = utils.Sprite('assets/sprites/mtlL3.png', 301, 443)
+        self.materialR1 = utils.Sprite('assets/sprites/mtlR1.png', 979, 292)
+        self.materialR2 = utils.Sprite('assets/sprites/mtlR2.png', 979, 365)
+        self.materialR3 = utils.Sprite('assets/sprites/mtlR3.png', 979, 443)
+        self.materialL1.opacity = 0
+        self.materialL2.opacity = 0
+        self.materialL3.opacity = 0
+        self.materialR1.opacity = 0
+        self.materialR2.opacity = 0
+        self.materialR3.opacity = 0
+
+        self.opt_knobs = utils.Sprite('assets/sprites/opt_knobs.png', 640, 325)
+        self.opt_buttons = utils.Sprite('assets/sprites/opt_buttons.png', 640, 447)
+        self.opt_knobs.opacity = 0
+        self.opt_buttons.opacity = 0
+
+
         self.sfx_tut_st = utils.get_sound('assets/audio/SFX/SFX_Tut_St.ogg')
         self.sfx_tut_print = utils.get_sound('assets/audio/SFX/SFX_Tut_Print.ogg')
+        self.sfx_tut_mat = utils.get_sound('assets/audio/SFX/SFX_Tut_Mat.ogg')
+        self.sfx_tut_opt = utils.get_sound('assets/audio/SFX/SFX_Tut_Opt.ogg')
+        self.sfx_tut_end = utils.get_sound('assets/audio/SFX/SFX_Tut_End.ogg')
 
 
         # second 0
@@ -67,6 +89,39 @@ class TutorialScene(SceneBase):
         self.AddTrigger(second, self.printer03, 'SetOpacity', 255)
 
         second = 10.5
+        self.AddTrigger(second, self.printer03, 'SetOpacity', 0)
+        self.AddTrigger(second, self.sfx_tut_mat, 'play')
+
+        self.AddTween('easeOutCubic', 0.5, self.materialL1, "opacity", 0, 255, second)
+        second = 11.5
+        self.AddTween('easeOutCubic', 0.5, self.materialL2, "opacity", 0, 255, second)
+        second = 12.5
+        self.AddTween('easeOutCubic', 0.5, self.materialL3, "opacity", 0, 255, second)
+        second = 13.5
+        self.AddTween('easeOutCubic', 0.5, self.materialR1, "opacity", 0, 255, second)
+        second = 14.5
+        self.AddTween('easeOutCubic', 0.5, self.materialR2, "opacity", 0, 255, second)
+        second = 15.5
+        self.AddTween('easeOutCubic', 0.5, self.materialR3, "opacity", 0, 255, second)
+
+        second = 16.5
+        self.AddTween('easeOutCubic', 0.5, self.materialL1, "opacity", 255, 0, second)
+        self.AddTween('easeOutCubic', 0.5, self.materialL2, "opacity", 255, 0, second)
+        self.AddTween('easeOutCubic', 0.5, self.materialL3, "opacity", 255, 0, second)
+        self.AddTween('easeOutCubic', 0.5, self.materialR1, "opacity", 255, 0, second)
+        self.AddTween('easeOutCubic', 0.5, self.materialR2, "opacity", 255, 0, second)
+        self.AddTween('easeOutCubic', 0.5, self.materialR3, "opacity", 255, 0, second)
+        
+        second = 18
+        self.AddTrigger(second, self.sfx_tut_opt, 'play')
+        self.AddTween('easeOutCubic', 4, self.opt_knobs, "opacity", 0, 255, second)
+        self.AddTween('easeOutCubic', 5, self.opt_buttons, "opacity", 0, 255, second)
+
+        second = 24
+        self.AddTrigger(second, self.sfx_tut_end, 'play')
+
+        
+
 
 
     
@@ -82,3 +137,14 @@ class TutorialScene(SceneBase):
         self.printer01.RenderWithAlpha(screen)
         self.printer02.RenderWithAlpha(screen)
         self.printer03.RenderWithAlpha(screen)
+
+        self.materialL1.RenderWithAlpha(screen)
+        self.materialL2.RenderWithAlpha(screen)
+        self.materialL3.RenderWithAlpha(screen)
+        self.materialR1.RenderWithAlpha(screen)
+        self.materialR2.RenderWithAlpha(screen)
+        self.materialR3.RenderWithAlpha(screen)
+        
+        self.opt_knobs.RenderWithAlpha(screen)
+        self.opt_buttons.RenderWithAlpha(screen)
+
