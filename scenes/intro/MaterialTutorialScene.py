9afcd6c381a6114e0ed8b15a8e1c0fe4957ec142
agar3s <ekeisco@gmail.com>
2019-03-17 17:48:46 -0700

    reduces the time of transition

--- scenes/intro/MaterialTutorialScene.py
+++ scenes/intro/MaterialTutorialScene.py
@@ -71,7 +71,7 @@ class MaterialTutorialScene(SceneBase):
 
     
     def Render(self, screen):
-        screen.fill((0x1B, 0x0C, 0x43))
+        screen.fill(constants.PALLETE_BACKGROUND_BLUE)
         self.subtitle_shadow.render_multiline(screen)
         self.subtitle.render_multiline(screen)
         graphics.render()
